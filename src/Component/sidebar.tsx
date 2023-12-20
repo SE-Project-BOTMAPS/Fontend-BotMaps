6676700cd0f841925a2458b031a87668c5171fa8
Pantagon <pichayoothunchainao@gmail.com>
2023-12-20 23:37:04 +0700

    change bg sidebar

--- src/Component/sidebar.tsx
+++ src/Component/sidebar.tsx
@@ -6,7 +6,8 @@ import { Button } from "primereact/button";
 function Sidebar() {
   return (
     <div className="">
-      <div className="flex flex-col bg-gradient-to-r from-cyan-300 to-blue-400 w-96 rounded-md px-4 py-8 my-8 mx-4">
+      {/* <div className="flex flex-col bg-gradient-to-r from-cyan-300 to-blue-400 w-96 rounded-md px-4 py-8 my-8 mx-4"> */}
+      <div className="flex flex-col bg-sky-400 w-96 rounded-md px-4 py-8 my-8 mx-4 shadow-2xl">
         <img src="../btlg1-removebg-preview.png" className="mb-8"></img>
         <div className="p-inputgroup flex-3 mb-8">
           <span className="p-input-icon-left">
