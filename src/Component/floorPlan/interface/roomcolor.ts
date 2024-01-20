67126b62478ec598377acbe23457c57b5859658f
Relpantagon <pichayoothunchainao@gmail.com>
2024-01-20 21:19:46 +0700

    Fix 412 Special to Deafult Room , Change color patlett, Remove Color hardcode

---
+++ src/Component/floorPlan/interface/roomcolor.ts
@@ -0,0 +1,12 @@
+export default {
+    Studycolor : "#D3DCE8",
+    Arjarncolor: "#E8E1D5",
+    Admincolor: "#FFE8BD",
+    Naksuksacolor : "#EDEDED",
+    Labroomcolor : "#DDD5D5",
+    Liftcolor : "#9EABD9",
+    Restroomcolor : "#9EABD9",
+    Nonecolor : "#4D4D4D",
+    Firecolor : "#ff8977",
+    Staircolor : "#566573"
+}
