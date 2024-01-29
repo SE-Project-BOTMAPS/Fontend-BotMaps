c4d7129bf1f34aea778a0daa2190b49a0848289f
Pantagon <pichayoothunchainao@gmail.com>
2024-01-29 23:37:20 +0700

    splitter fix

--- src/Frame/index.tsx
+++ src/Frame/index.tsx
@@ -1,24 +1,21 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import "./Frame.css";
 import { Splitter, SplitterPanel } from 'primereact/splitter';
 import LeftSideFrame from './../Pages/LeftsideFrame/index';
-        
+
 const Frame = ({ children }: { children?: ReactNode }) => {
   return (
-    <Splitter style={{ height: '100vdh' }}>
-            <SplitterPanel className="">
-              <LeftSideFrame/>
-            </SplitterPanel>
-            {/* <SplitterPanel className=""></SplitterPanel> */}
-            <SplitterPanel className="">{children}</SplitterPanel>
-        </Splitter>
-    // <div className="flex">
-    //   <div className="Frame-Leftside">
-    //     <LeftSideFrame />
-    //   </div>
-    //   <div className="Frame-Floorplan">{children}</div>
-      
-    // </div>
+    <div className="splitter-container">
+      <Splitter>
+        <SplitterPanel className="splitter-panel-left" size={75}>
+          <LeftSideFrame />
+        </SplitterPanel>
+        <SplitterPanel className="splitter-panel-right">
+          {children}
+        </SplitterPanel>
+      </Splitter>
+    </div>
   );
 };
+
 export default Frame;
