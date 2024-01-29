f7634de68a851bbd91491651423f55b2dbb43dc1
Pantagon <pichayoothunchainao@gmail.com>
2024-01-29 22:47:23 +0700

    Add Spitter

--- src/Frame/index.tsx
+++ src/Frame/index.tsx
@@ -1,15 +1,24 @@
 import { ReactNode } from "react";
-import LeftSideFrame from "../Pages/LeftsideFrame";
 import "./Frame.css";
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+import LeftSideFrame from './../Pages/LeftsideFrame/index';
+        
 const Frame = ({ children }: { children?: ReactNode }) => {
   return (
-    <div className="flex">
-      <div className="Frame-Leftside">
-        <LeftSideFrame />
-      </div>
-      <div className="Frame-Floorplan">{children}</div>
+    <Splitter style={{ height: '100vdh' }}>
+            <SplitterPanel className="">
+              <LeftSideFrame/>
+            </SplitterPanel>
+            {/* <SplitterPanel className=""></SplitterPanel> */}
+            <SplitterPanel className="">{children}</SplitterPanel>
+        </Splitter>
+    // <div className="flex">
+    //   <div className="Frame-Leftside">
+    //     <LeftSideFrame />
+    //   </div>
+    //   <div className="Frame-Floorplan">{children}</div>
       
-    </div>
+    // </div>
   );
 };
 export default Frame;
