25d484ff8183f06b6b5b0c21ac2ac8e35fac8b67
AuJung <90750969+AuJUngZ@users.noreply.github.com>
2024-02-25 21:07:33 +0700

    change to native name
    * change to native name

--- state/slices/types/room.type.ts
+++ state/slices/types/room.type.ts
@@ -1,42 +1,43 @@
 export type ResponseRoomEvent = {
-    id: string,
-    title: string,
-    code: string,
-    day: string,
-    start_time: string,
-    end_time: string,
-    location_id: string,
-    professor_id: string,
-    Location: {
-        id: string,
-        location: string,
-        detail: string
-    },
-    Professor: {
-        id: string,
-        data_who: string,
-        full_name: string
-    }
-}
+  id: string;
+  title: string;
+  code: string;
+  day: string;
+  start_time: string;
+  end_time: string;
+  location_id: string;
+  professor_id: string;
+  Location: {
+    id: string;
+    location: string;
+    detail: string;
+  };
+  Professor: {
+    id: string;
+    data_who: string;
+    full_name: string;
+  };
+};
 
 type ResponseDay = {
-    mon : ResponseRoomEvent[],
-    tue : ResponseRoomEvent[],
-    wed : ResponseRoomEvent[],
-    thu : ResponseRoomEvent[],
-    fri : ResponseRoomEvent[],
-    sat : ResponseRoomEvent[],
-    sun : ResponseRoomEvent[]
-}
+  mon: ResponseRoomEvent[];
+  tue: ResponseRoomEvent[];
+  wed: ResponseRoomEvent[];
+  thu: ResponseRoomEvent[];
+  fri: ResponseRoomEvent[];
+  sat: ResponseRoomEvent[];
+  sun: ResponseRoomEvent[];
+};
 
 export type office = {
-    data_who: string
-    full_name : string
-}
+  data_who: string;
+  full_name: string;
+  native_name: string;
+};
 
 export type EventKey = keyof ResponseDay;
 
 export type Response = {
-    events: ResponseDay
-    officeOf : office[]
-}
+  events: ResponseDay;
+  officeOf: office[];
+};
