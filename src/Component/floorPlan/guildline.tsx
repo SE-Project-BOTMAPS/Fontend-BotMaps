c9de53e115a8830f1694ee057131f0b2a6ea4d1c
Pantagon <pichayoothunchainao@gmail.com>
2024-02-26 23:32:11 +0700

    rgb button seleted, border guideline

--- src/Component/floorPlan/guildline.tsx
+++ src/Component/floorPlan/guildline.tsx
@@ -19,10 +19,10 @@ const colorGuideline: ColorGuideline = generateColorGuideline(roomcolor);
 
 const Guildline: React.FC = () => {
   return (
-    <div className="grid grid-cols-5 mx-auto pt-4 ml-4">
+    <div className="grid grid-cols-5 mx-10 p-4 border border-solid border-black rounded-2xl bg-white">
       {Object.entries(colorGuideline).map(([name, color]) => (
-        <div key={name} className="flex m-1">
-          <div style={{ backgroundColor: color, width: '15px', height: '15px', borderRadius: '50%' ,margin:'auto 0'}}></div>
+        <div key={name} className="flex m-1 justify-center">
+          <div style={{ backgroundColor: color, width: '15px', height: '15px', borderRadius: '50%', margin: 'auto 0', border: '1px solid #000' }}></div>
           <div className="text-sm mx-1">{name}</div>
         </div>
       ))}
