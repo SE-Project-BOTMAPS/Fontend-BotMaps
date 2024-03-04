e650a277e7a251acc8a4348897c011bb2ba0ec89
PANTAGON <90751052+realpantagon@users.noreply.github.com>
2024-03-04 14:56:41 +0700

    Create route for Signage (#32)
    * Add Route for Signage
    
    * Remove unuse

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { PrimeReactProvider } from 'primereact/api';
+import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-blue/theme.css";
-import {Provider} from "react-redux";
-import {store} from "../state/store.ts";
+import { Provider } from "react-redux";
+import { store } from "../state/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <PrimeReactProvider>
     <React.StrictMode>
-        <Provider store={store}>
+      <Provider store={store}>
+        <BrowserRouter>
           <App />
-        </Provider>
+        </BrowserRouter>
+      </Provider>
     </React.StrictMode>
   </PrimeReactProvider>
 );
